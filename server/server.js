62d4ec205096ea69f46687edfd1a2c00ddcd3c2d
MaryBrantnall <marybrantnall@gmail.com>
2025-12-08 18:36:54 -0400

    port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const Rsvp = require('./models/Rsvp');
 const validateRsvp = require('./middleware/validateMiddleware')
 const cors = require('cors');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
