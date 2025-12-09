01914ed840bc6bfb06360656b9dcf32889ad4994
MaryBrantnall <marybrantnall@gmail.com>
2025-12-08 23:20:52 -0400

    what

--- server/validation/rsvpSchema.js
+++ server/validation/rsvpSchema.js
@@ -5,12 +5,12 @@ const rsvpSchema = Joi.object({
 
     name: Joi.string().trim().max(75).required(),
     email: Joi.string().trim().email().lowercase(),
-    attending: Joi.boolean().required(),
+    attending: Joi.boolean().optional(),
     plusOne: Joi.boolean().optional(),
     songChoice: Joi.string().trim().max(100).default('').optional(),
     dietaryRestrictions: Joi.string().trim().max(200).default('').optional(),
     comments: Joi.string().trim().max(400).default('').optional(),
-    isBringingSpecFood: Joi.boolean(),
+    isBringingSpecFood: Joi.boolean().optional(),
 
     foodOption: Joi.string().when('isBringingSpecFood', {
         is: true,
