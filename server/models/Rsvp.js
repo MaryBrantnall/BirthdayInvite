718227d4c75445c4c2f9717376d06e025556e9c0
MaryBrantnall <marybrantnall@gmail.com>
2025-12-08 23:14:34 -0400

    schema fixes

--- server/models/Rsvp.js
+++ server/models/Rsvp.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const RsvpSchema = new mongoose.Schema({
     name: {type: String,required: true, trim: true, maxlength: 75},
     email: {type: String, trim: true, match: [/.+\@.+\..+/, 'Please fill a valid email address'], lowercase: true},
-    attending: {type: Boolean, required: true},
-    plusOne: {type: Boolean, required: true},
+    attending: {type: Boolean},
+    plusOne: {type: Boolean},
     songChoice: {type: String, maxlength: 100, trim: true, default: ''},
     dietaryRestrictions: {type: String, maxlength: 200, trim: true, default: ''},
     comments: {type: String, maxlength: 400, trim: true, default: ''},
-    isBringingSpecFood: {type: Boolean, required: true},
+    isBringingSpecFood: {type: Boolean},
     foodOption: {type: String},
 },
 { collection: 'party_rsvps', timestamps: true });
--- server/validation/rsvpSchema.js
+++ server/validation/rsvpSchema.js
@@ -6,10 +6,10 @@ const rsvpSchema = Joi.object({
     name: Joi.string().trim().max(75).required(),
     email: Joi.string().trim().email().lowercase(),
     attending: Joi.boolean().required(),
-    plusOne: Joi.boolean(),
-    songChoice: Joi.string().trim().max(100).default(''),
-    dietaryRestrictions: Joi.string().trim().max(200).default(''),
-    comments: Joi.string().trim().max(400).default(''),
+    plusOne: Joi.boolean().optional(),
+    songChoice: Joi.string().trim().max(100).default('').optional(),
+    dietaryRestrictions: Joi.string().trim().max(200).default('').optional(),
+    comments: Joi.string().trim().max(400).default('').optional(),
     isBringingSpecFood: Joi.boolean(),
 
     foodOption: Joi.string().when('isBringingSpecFood', {
